dbe10ac2805c6785960d27db45138d76ca0d42a7
MatheusSilva20 <110861566+MatheusSilva20@users.noreply.github.com>
2022-09-15 15:47:39 -0300

    Add files via upload

---
+++ SaidaDados/SaidaDados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace SaidaDados { 
+    class Program{
+        static void Main(string[] args) {
+
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+            Console.WriteLine("Com quebra de linha");//O WriteLine efetua a quebra de linha
+            Console.Write("Sem quebra de linha");// Quando somente Write ele não quebra a linha
+            Console.WriteLine("Com quebra de linha");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));//To.String limita o numero a ser mostrado na saida de dados sendo F2 dois numeros após a vírgula e arredondado
+            Console.WriteLine(saldo.ToString("F4"));//To.String limita o numero a ser mostrado na saida de dados sendo F4 quatro numeros após a vírgula e arredondado
+            Console.WriteLine(saldo.ToString("F4",CultureInfo.InvariantCulture));//InvariantCulture é um separador de decimais
+        }   
+
+    }
+}
---
+++ SaidaDados2/SaidaDados2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDados2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome, idade, saldo);//Saída de Dados com PlaceHolders
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");//Saída de Dados com Interpolação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");//Saída de Dados com Concatenação
+        }
+    }
+}
