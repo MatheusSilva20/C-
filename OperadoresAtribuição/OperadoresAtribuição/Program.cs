fda09cea77810921d50d05a40db7bd54455b65e9
MatheusSilva20 <110861566+MatheusSilva20@users.noreply.github.com>
2022-09-15 15:43:34 -0300

    Add files via upload

---
+++ OperadoresAtribuição/OperadoresAtribuição/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperadoresAtribuição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);//pega a variável e soma com o 10
+
+            a *= 3; Console.WriteLine(a);//ele pega o resultado do a+=2 e faz a * 3 
+
+            string s = "ABC";
+            Console.WriteLine(s);// += funciona com letras tambem
+
+            s += "DEF";
+            Console.WriteLine(s);
+        }
+    }
+}
