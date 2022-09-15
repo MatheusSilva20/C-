499dc0ab22596b4a230782b51c8e9a1cb2e7c259
MatheusSilva20 <110861566+MatheusSilva20@users.noreply.github.com>
2022-09-15 15:39:31 -0300

    Add files via upload

---
+++ for/for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace @for
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Quantos números inteiros vc vai digitar? ");
+            int n1 = int.Parse(Console.ReadLine());
+
+
+            int soma = 0;
+            for (int i = 1; i <= n1; i++) {
+                Console.Write("Valor: #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
