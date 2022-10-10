9c80f965081e34d2956a9684af1704732c87440e
MatheusSilva20 <110861566+MatheusSilva20@users.noreply.github.com>
2022-10-10 15:11:33 -0300

    Add files via upload

---
+++ Out/Out/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Out
+{
+     class Calculator{
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
