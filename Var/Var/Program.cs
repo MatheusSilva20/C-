991fd59d358a239abfe7e1269e022b6c4395a80b
MatheusSilva20 <110861566+MatheusSilva20@users.noreply.github.com>
2022-10-14 14:06:59 -0300

    Add files via upload

---
+++ Var/Var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Var{
+    internal class Program{
+        static void Main(string[] args){
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
